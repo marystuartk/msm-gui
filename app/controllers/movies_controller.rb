feab6439a8c300f47aa14947386cea110077f031
marystuartk <164695576+marystuartk@users.noreply.github.com>
2024-04-29 21:11:14 +0000

    movies is done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,23 @@
 class MoviesController < ApplicationController
+
+  def update
+    m_id = params.fetch("the_id")
+    
+    matching_records = Movie.where({ :id => m_id })
+    the_movie = matching_records.at(0)
+
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    the_movie.save
+
+    redirect_to("/movies/#{the_movie.id}")
+
+  end
+
   def create
     m = Movie.new
     m.title = params.fetch("the_title")
@@ -8,6 +27,18 @@ class MoviesController < ApplicationController
     m.director_id = params.fetch("the_director_id")
 
     m.save
+
+    redirect_to("/movies")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id })
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
 
     redirect_to("/movies")
   end
